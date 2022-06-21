995c7421c6cd30bec410b2ff83649d690531cdea
Buidhww <106917589+Buidhww@users.noreply.github.com>
2022-06-21 14:58:28 +0800

    Create MainABCDEFU.lua

---
+++ MainABCDEFU.lua
@@ -0,0 +1,6 @@
+-- Join The Discord https://discord.gg/Fm6gAjRcP9
+-- script to set the sheet to play
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Quyenday1/ok2/main/Abcdefu"))()
+
+-- main
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nameless9000/RobloxPianoAutoplayer/main/script.lua"))()
