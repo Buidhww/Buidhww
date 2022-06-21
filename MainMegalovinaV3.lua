b4062b654591933d9e72cd22ba9f04652ab3d342
Buidhww <106917589+Buidhww@users.noreply.github.com>
2022-06-21 19:57:37 +0800

    Update and rename MainSongMegalovina.lua to MainMegalovinaV3.lua

---
+++ MainMegalovinaV3.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Quyenday1/oooooooo/main/Ok"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nameless9000/RobloxPianoAutoplayer/main/script.lua"))()
--- MainSongMegalovina.lua
+++
@@ -1,6 +0,0 @@
--- Join The Discord https://discord.gg/Fm6gAjRcP9
--- script to set the sheet to play
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/Megalovania.lua"))()
-
--- main
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Nameless9000/RobloxPianoAutoplayer/main/script.lua"))()
