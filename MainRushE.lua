1fae82ed78f3dfc56f673f75ed90085b53388b30
Buidhww <106917589+Buidhww@users.noreply.github.com>
2022-06-21 14:39:35 +0800

    Create MainRushE.lua

---
+++ MainRushE.lua
@@ -0,0 +1,6 @@
+-- Join The Discord https://discord.gg/Fm6gAjRcP9
+-- script to set the sheet to play
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Quyenday1/ok1/main/Ok1"))()
+
+-- main
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nameless9000/RobloxPianoAutoplayer/main/script.lua"))()
