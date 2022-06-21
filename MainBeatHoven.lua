9167174271163d1f39d851c81abb299bba4d553d
Buidhww <106917589+Buidhww@users.noreply.github.com>
2022-06-21 16:50:09 +0800

    Create MainBeatHoven.lua

---
+++ MainBeatHoven.lua
@@ -0,0 +1,7 @@
+HERE IS ABCDEFU SONG
+-- Join The Discord https://discord.gg/Fm6gAjRcP9
+-- script to set the sheet to play
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/Beethoven.lua"))()
+
+-- main
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nameless9000/RobloxPianoAutoplayer/main/script.lua"))()
