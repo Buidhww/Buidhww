42cfe79caa08b4a72bfbf7fa112003fb85e589a7
Buidhww <106917589+Buidhww@users.noreply.github.com>
2022-06-21 16:24:31 +0800

    Create GiornoWind.lua

---
+++ GiornoWind.lua
@@ -0,0 +1,10 @@
+-- Sheet for Rondo Alla Turca by Wolfgang Amadeus Mozart
+
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 2*60 + 40 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- tempo overides the delay
+
+shared.scr = [[[r7] r r e r y r Q e [r7] r r e r i u y e [r7] r r e r y r Q e [r7] r r e r y  i u y e G d dfgfdSdf G k a S dfdSjh G d dfgfdSdf G k k L zhGgzj[akr]
+]]
