2a11e1433dac087c66b864885f4b7f44ec1ffb8e
Buidhww <106917589+Buidhww@users.noreply.github.com>
2022-06-22 21:25:45 +0800

    Create Stay with me by miki matsubara.lua

---
+++ Stay with me by miki matsubara.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Quyenday1/stay-with-me/main/Stay%20with%20me"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nameless9000/RobloxPianoAutoplayer/main/script.lua"))()
