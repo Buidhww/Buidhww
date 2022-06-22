83fa7ba413e1f1f3aa701d79cc118e1fafa7b6fb
Buidhww <106917589+Buidhww@users.noreply.github.com>
2022-06-22 12:02:01 +0800

    Create RickrollV3.lua

---
+++ RickrollV3.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Quyenday1/rick-roll/main/Rick%20roll"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nameless9000/RobloxPianoAutoplayer/main/script.lua"))()
