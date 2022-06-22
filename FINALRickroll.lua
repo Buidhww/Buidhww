4d7fb2e6b8b6b914bd67b35e6d625d18d630ddc2
Buidhww <106917589+Buidhww@users.noreply.github.com>
2022-06-22 12:04:03 +0800

    Create FINALRickroll.lua

---
+++ FINALRickroll.lua
@@ -0,0 +1,62 @@
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+
+local Window = Library.CreateLib("Piano | AutoPlayer V3 | By Bacon", "DarkTheme")
+
+local Tab = Window:NewTab("Autoplayer")
+local SectionTab1 = Tab:NewSection("Click Any Song This is still Beta One time only tap")
+
+local Tab = Window:NewTab("Updates News")
+local SectionTab2 = Tab:NewSection("!!!!NEW UPDATE!!!!")
+
+local Tab = Window:NewTab("Credits")
+local SectionTab3 = Tab:NewSection("Thx If Ur Reading the Credits True legend")
+
+SectionTab1:NewButton("Gio Wind Theme", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/MainMegalovinaV3.lua"))()
+end)
+
+SectionTab1:NewButton("MoonLightSoNata", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/MainsongMoonLight.lua"))()
+end)
+
+SectionTab1:NewButton("Blood In The Water", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/MainSongBloodWater"))()
+end)
+
+SectionTab1:NewButton("Rush E [FAST VERSION]", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/MainRushE.lua"))()
+end)
+
+SectionTab1:NewButton("Rickroll [May Not Work Properly]", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/MainRushE.lua"))()
+end)
+
+SectionTab1:NewButton("ABCDEFU [Working Properly]", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/MainABCDEFU.lua"))()
+end)
+
+SectionTab2:NewLabel("Removed Rickroll Song")
+
+SectionTab2:NewLabel("Added Rush E")
+
+SectionTab2:NewLabel("Added ABCDEFU")
+
+SectionTab2:NewLabel("Removed Megalovania")
+
+SectionTab2:NewLabel("Added Gio Wind Theme")
+
+SectionTab2:NewLabel("Added Rickroll [May Be Bugged Again]")
+
+SectionTab2:NewLabel("Tryna Fix Megalovania")
+
+SectionTab2:NewLabel("Move Able Gui Added")
+
+SectionTab2:NewLabel("Autoplayer Added Speed")
+
+SectionTab3:NewLabel("Owner By BaconKing Dont obey plz")
+
+SectionTab3:NewLabel("Thanks To Quyền#6396(: ")
+
+SectionTab3:NewLabel("Subscribe To JN HH Gaming Hes Epic and cool")
+
+SectionTab3:NewLabel("THIS IS JUST A BETA MAY NOT WORK PROPERLY")
