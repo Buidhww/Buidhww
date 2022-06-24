aa43b34658ddfd365bf554ef5de9ede8e5e9735a
Buidhww <106917589+Buidhww@users.noreply.github.com>
2022-06-24 15:28:25 +0800

    Create AutoPlayerPianoNewUpdateV4.lua

---
+++ AutoPlayerPianoNewUpdateV4.lua
@@ -0,0 +1,91 @@
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+
+local Window = Library.CreateLib("Piano | AutoPlayer V4 | By CarlNPCuwu#9433", "DarkTheme")
+
+local Tab = Window:NewTab("Autoplayer")
+local SectionTab1 = Tab:NewSection("Click Any Song This is still Beta One time only tap")
+
+local Tab = Window:NewTab("Updates News")
+local SectionTab2 = Tab:NewSection("!!!!NEW UPDATE!!!!")
+
+local Tab = Window:NewTab("Maybe Leaks (:")
+local SectionTab3 = Tab:NewSection("Leaks What will be V5")
+
+local Tab = Window:NewTab("Autoplayer")
+local SectionTab4 = Tab:NewSection("Click Any Song This is still Beta One time only tap")
+
+local Tab = Window:NewTab("Credits")
+local SectionTab5 = Tab:NewSection("Thx If Ur Reading the Credits True legend")
+
+local Tab = Window:NewTab("Setting")
+local SectionTab6 = Tab:NewSection("This Channel is useless")
+
+SectionTab1:NewButton("Gio Wind Theme", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/MainMegalovinaV3.lua"))()
+end)
+
+SectionTab1:NewButton("MoonLightSoNata", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/MainsongMoonLight.lua"))()
+end)
+
+SectionTab1:NewButton("Blood In The Water", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/MainSongBloodWater"))()
+end)
+
+SectionTab1:NewButton("Rush E [FAST VERSION]", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/MainRushE.lua"))()
+end)
+
+SectionTab1:NewButton("Rickroll [May Not Work Properly]", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/MainRushE.lua"))()
+end)
+
+SectionTab1:NewButton("ABCDEFU [Working Properly]", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/MainABCDEFU.lua"))()
+end)
+
+SectionTab1:NewButton("miki matsubara [Working Properly]", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Buidhww/Buidhww/main/Stay%20with%20me%20by%20miki%20matsubara.lua"))()
+end)
+
+SectionTab2:NewLabel("Also Fixed Rickrolled a bit")
+
+SectionTab2:NewLabel("Added Rush E")
+
+SectionTab2:NewLabel("Added ABCDEFU")
+
+SectionTab2:NewLabel("Added Rejoin Button")
+
+SectionTab2:NewLabel("Added Gio Wind Theme")
+
+SectionTab2:NewLabel("Added Rickroll [May Be Bugged Again]")
+
+SectionTab2:NewLabel("Tryna Fix Megalovania")
+
+SectionTab2:NewLabel("Added miki matsubara StayWithMe")
+
+SectionTab2:NewLabel("Move Able Gui Added")
+
+SectionTab2:NewLabel("Autoplayer Added Speed")
+
+SectionTab3:NewLabel("New UI In V5?!?¿??!?! Maybe")
+
+SectionTab3:NewLabel("New Function stuff Maybe In The Future.")
+
+SectionTab4:NewButton("Hopefully i can add rejoin button", "ButtonInfo", function()
+    print("Epic")
+end)
+
+SectionTab5:NewLabel("Owner By BaconKing Dont obey plz")
+
+SectionTab5:NewLabel("Thanks To Quyền#6396(: ")
+
+SectionTab5:NewLabel("Subscribe To JN HH Gaming Hes Epic and cool")
+
+SectionTab5:NewLabel("THIS IS JUST A BETA MAY NOT WORK PROPERLY")
+
+SectionTab6:NewLabel("Never Gonna Give U Up")
+
+SectionTab6:NewLabel("Never gonna let u down")
+
+SectionTab6:NewLabel("Get RickRolled LOLOL")
