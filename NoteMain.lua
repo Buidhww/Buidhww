eed984f378300f6e2bdc5e10f613cb4c2dc8dfcf
Buidhww <106917589+Buidhww@users.noreply.github.com>
2022-06-24 23:21:32 +0800

    Create NoteMain.lua

---
+++ NoteMain.lua
@@ -0,0 +1,8 @@
+shared.stop = true -- stops the player at any time if true
+-- CONFIG:
+shared.ftime = 2*60 + 00 -- time in seconds for the song to finish (extended by |)
+shared.delay = nil -- delay overides the ftime
+shared.tempo = nil -- tempo overides the delay
+
+shared.scr = [[ d f j k d d d f k k k k j
+]]
